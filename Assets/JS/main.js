dc6e11bac254f71a4c3272ce5f3abcb78262b63f
Lahfir <nmhlahfir2@gmail.com>
2021-06-24 10:55:28 +0530

    toggle-changes

---
+++ Assets/JS/main.js
@@ -0,0 +1,9 @@
+$("#gpa-toggle").click(function () {
+  $("#gpa-toggle").addClass("selected-toggle");
+  $("#cgpa-toggle").removeClass("selected-toggle");
+});
+
+$("#cgpa-toggle").click(function () {
+  $("#cgpa-toggle").addClass("selected-toggle");
+  $("#gpa-toggle").removeClass("selected-toggle");
+});
