b9a47726c12e33b6efec10d91ce769b1adbba3a7
Lahfir <nmhlahfir2@gmail.com>
2021-06-26 11:12:38 +0530

    lasjd

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,15 +1,15 @@
 $("#gpa-toggle").click(function () {
   $("#gpa-toggle").addClass("selected-toggle");
   $("#cgpa-toggle").removeClass("selected-toggle");
-  $(".cgpa-content").fadeOut(400);
-  $(".gpa-content").fadeIn(400);
+  $(".cgpa-content").css("display", "none");
+  $(".gpa-content").css("display", "flex");
 });
 
 $("#cgpa-toggle").click(function () {
   $("#cgpa-toggle").addClass("selected-toggle");
   $("#gpa-toggle").removeClass("selected-toggle");
-  $(".cgpa-content").fadeIn(400);
-  $(".gpa-content").fadeOut(400);
+  $(".gpa-content").css("display", "none");
+  $(".cgpa-content").css("display", "flex");
 });
 
 $("#dept-select").click(function () {
