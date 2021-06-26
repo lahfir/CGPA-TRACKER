b6f343ebaf3664ead64cc1df87c8d890975666c1
Lahfir <nmhlahfir2@gmail.com>
2021-06-26 09:54:57 +0530

    select

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -8,4 +8,20 @@ $("#cgpa-toggle").click(function () {
   $("#gpa-toggle").removeClass("selected-toggle");
 });
 
+$("#dept-select").click(function () {
+  var newOptions = {
+    9: "values-9",
+    10: "values-10",
+  };
 
+  if ($("#dept-select").val() == 22) {
+    var $el = $("#sem-select");
+    if ($("#sem-select> option").length < 10) {
+      $.each(newOptions, function (key, value) {
+        $el.append($("<option></option>").attr("value", value).text(key));
+      });
+    }
+  } else {
+    $("#sem-select option:gt(7)").remove();
+  }
+});
