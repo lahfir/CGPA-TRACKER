7a119c3f0cb3b2c76123bd615725473d2631f8d7
Lahfir <nmhlahfir2@gmail.com>
2021-07-02 22:43:21 +0530

    static-server-files

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port=3000
-const mongoose = require('mongoose')
-var sample = require('./fetch')
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+var sample = require("./fetch");
 
-var db= mongoose.connect('mongodb+srv://cgpatrack:cgpa24jun@cgpa.oc0rt.mongodb.net/CGPA_TRACKER', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-})
+var db = mongoose.connect(
+  "mongodb+srv://cgpatrack:cgpa24jun@cgpa.oc0rt.mongodb.net/CGPA_TRACKER",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
+
+app.use(express.static("Public"));
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
-sample.find({"Semester":"2"},function(err,docs){
-    if(err)
-        console.log("error");
-    else
-        console.log(docs);
-})
+sample.find({ Semester: "2" }, function (err, docs) {
+  if (err) console.log("error");
+  else console.log(docs);
+});
