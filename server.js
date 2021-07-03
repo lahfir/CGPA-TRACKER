e31d0b0e2aeea78255b0119e840e4db74ca1cd92
Lahfir <nmhlahfir2@gmail.com>
2021-07-03 17:03:54 +0530

    Process

--- server.js
+++ server.js
@@ -1,48 +1,41 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
 const mongoose = require("mongoose");
 var sample = require("./fetch");
-const bodyParser = require('body-parser')
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true }))
-var Sem=1;
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+var Sem = 1;
+require("dotenv").config();
 
-var db = mongoose.connect(
-  "mongodb+srv://cgpatrack:cgpa24jun@cgpa.oc0rt.mongodb.net/CGPA_TRACKER",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  }
-);
+var db = mongoose.connect(process.env.CONNECT, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.use(express.static("Public"));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
 
-async function findOne(){
-    await mongoose.connect('mongodb+srv://cgpatrack:cgpa24jun@cgpa.oc0rt.mongodb.net/CGPA_TRACKER', {
+async function findOne() {
+  await mongoose.connect(process.env.CONNECT, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    })
-    sample.find({"Semester": Sem},function(err,docs){
-        if(err)
-            console.log(err);
-        else
-            console.log("it works");
-    })
+  });
+  sample.find({ Semester: Sem }, function (err, docs) {
+    if (err) console.log(err);
+    else console.log("it works");
+  });
 }
 findOne();
 
-
-app.post('/api/selectDept',async(req,res)=>{
-    var Val = req.body
-    console.log(Val)
-})
-
-
+app.post("/api/selectDept", async (req, res) => {
+  var Val = req.body;
+  console.log(Val);
+});
